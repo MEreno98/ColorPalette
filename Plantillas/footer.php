f69eb2f007d15749ff95d486c85bd7112f5fc61b
Ruben Polo <polouters@gmail.com>
2016-02-03 23:23:38 +0100

    Actualizado el Copirai

--- Plantillas/footer.php
+++ Plantillas/footer.php
@@ -1,10 +1,10 @@
 </div>
 <footer class="footer">
     <div class="container">
-        <div class="col-sm-6 col-xs-12"><p class="text-muted">Place sticky footer content here.</p></div>
+        <div class="col-sm-6 col-xs-12"><p class="text-muted"> </p></div>
         <div class="col-sm-6 col-xs-12">
-            <p class="text-muted text-right hidden-xs"> ColorssPalette © <?php  echo date("Y")?></p>
-            <p class="text-muted  hidden-md hidden-lg hidden-sm"> ColorssPalette © <?php  echo date("Y")?></p>
+            <p class="text-muted text-right hidden-xs"> Copyright © <?php  echo date("Y")?> ColorssPalette </p>
+            <p class="text-muted  hidden-md hidden-lg hidden-sm"> Copyright © <?php  echo date("Y")?> ColorssPalette</p>
         </div>
     </div>
 </footer>
